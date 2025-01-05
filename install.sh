f4dad98bf14001491580831d1c4e2476ce346ecd
Aaron Marburg <amarburg@uw.edu>
2025-01-05 12:02:26 -0800

    Correctly set permissions in install script

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 #
 # Install script which is added to install.tar.bz2 by "make package"
 
-sudo install -o root -g root boot/* /boot
-sudo find lib/modules/5.15.148-tegra/updates/ -type f -print -exec install -o root -g root \{\} /\{\} \;
+sudo install -o root -g root -m 644 boot/* /boot
+sudo find lib/modules/5.15.148-tegra/updates/ -type f -print -exec install -D -o root -g root -m 644 \{\} /\{\} \;
 sudo depmod -a
