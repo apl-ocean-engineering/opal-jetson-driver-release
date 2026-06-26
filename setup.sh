92b47b3607070472825669dcccac23848fe531b4
Aaron Marburg <amarburg@uw.edu>
2026-06-25 22:00:22 -0700

    Initial commit of generic/l4t-36.5.0

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@ export ARCH=arm64
 
 # Assumes the cross compiler package has been unpacked in the current directory;
 # see the README
-export CROSS_COMPILE=$(pwd)/x-tools/aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+export CROSS_COMPILE=$(pwd)/aarch64--glibc--stable-2022.08-1/bin/aarch64-linux-
 
 # Assumes the BSP has been unpacked in the current directory,
 # and kernel_sources.tar.bz2 has been unpacked in Linux_for_Tegra/kernel/
 # see the README
 #export KERNEL_HEADERS=$(pwd)/Linux_for_Tegra/source/kernel/kernel-noble/
 
-export KERNEL_HEADERS=$(pwd)/Linux_for_Tegra/kernel/linux-headers-6.8.12-1021-tegra-linux_x86_64/3rdparty/canonical/linux-noble/
-export kernel_name=noble
+export KERNEL_HEADERS=$(pwd)/Linux_for_Tegra/kernel/linux-headers-5.15.185-tegra-linux_x86_64/3rdparty/canonical/linux-jammy/kernel-source/
+export kernel_name=jammy
 
 export INSTALL_MOD_PATH=$(pwd)/install
